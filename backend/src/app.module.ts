1f8a4804511e7f401677ab28970ab2629803d771
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-11-28 17:46:16 +0100

    Add empty database service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { DatabaseService } from './database.service'
 import { Module } from '@nestjs/common'
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DatabaseService],
 })
 export class AppModule {}
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,11 @@
+import { DatabaseService } from './database.service'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
   getHello(): string {
-    return 'Hello World!'
+    return this.databaseService.test()
   }
 }
