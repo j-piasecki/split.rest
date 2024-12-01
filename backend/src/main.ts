bca37729d114cb57730e3cd27cb76423f5db9984
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-12-01 17:50:29 +0100

    Setup https

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,21 @@
 import { AppModule } from './app.module'
 import { NestFactory } from '@nestjs/core'
+import * as fs from 'fs'
+
+const IS_DEV = process.env.DEV === '1'
+
+const httpsOptions = IS_DEV
+  ? undefined
+  : {
+      key: fs.readFileSync('./secrets/private-key.pem'),
+      cert: fs.readFileSync('./secrets/public-certificate.pem'),
+    }
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: httpsOptions,
+  })
   app.enableCors()
-  await app.listen(process.env.PORT ?? 3000)
+  await app.listen(IS_DEV ? 3000 : 443)
 }
 bootstrap()
