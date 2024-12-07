ec4653b10d82c650a3d88890e48dced66d67796c
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-12-07 16:08:42 +0100

    Make all columns not null

--- backend/src/database/createDatabase.ts
+++ backend/src/database/createDatabase.ts
@@ -4,9 +4,9 @@ export async function createDatabase(pool: Pool) {
   await pool.query(`
     CREATE TABLE IF NOT EXISTS users(
       id VARCHAR(32) PRIMARY KEY,
-      name VARCHAR(128),
-      email VARCHAR(512),
-      created_at bigint,
+      name VARCHAR(128) NOT NULL,
+      email VARCHAR(512) NOT NULL,
+      created_at bigint NOT NULL,
       photo_url VARCHAR(512) NULL
     )
   `)
@@ -14,11 +14,12 @@ export async function createDatabase(pool: Pool) {
   await pool.query(`
     CREATE TABLE IF NOT EXISTS groups(
       id SERIAL PRIMARY KEY,
-      name VARCHAR(128),
-      created_at bigint,
-      currency VARCHAR(8),
-      owner VARCHAR(32),
-      deleted BOOLEAN DEFAULT FALSE,
+      name VARCHAR(128) NOT NULL,
+      created_at bigint NOT NULL,
+      currency VARCHAR(8) NOT NULL,
+      owner VARCHAR(32) NOT NULL,
+      total DECIMAL(10, 2) NOT NULL DEFAULT 0,
+      deleted BOOLEAN NOT NULL DEFAULT FALSE,
 
       FOREIGN KEY (owner) REFERENCES users(id)
     )
@@ -26,12 +27,12 @@ export async function createDatabase(pool: Pool) {
 
   await pool.query(`
     CREATE TABLE IF NOT EXISTS group_members(
-      group_id INTEGER,
-      user_id VARCHAR(32),
-      balance DECIMAL(10, 2),
-      is_admin BOOLEAN,
-      has_access BOOLEAN,
-      is_hidden BOOLEAN,
+      group_id INTEGER NOT NULL,
+      user_id VARCHAR(32) NOT NULL,
+      balance DECIMAL(10, 2) NOT NULL,
+      is_admin BOOLEAN NOT NULL,
+      has_access BOOLEAN NOT NULL,
+      is_hidden BOOLEAN NOT NULL,
 
       PRIMARY KEY (group_id, user_id),
       FOREIGN KEY (group_id) REFERENCES groups(id),
@@ -42,14 +43,14 @@ export async function createDatabase(pool: Pool) {
   await pool.query(`
     CREATE TABLE IF NOT EXISTS splits(
       id SERIAL PRIMARY KEY,
-      group_id INTEGER,
-      total DECIMAL(10, 2),
-      paid_by VARCHAR(32),
-      created_by VARCHAR(32),
-      name VARCHAR(512),
-      timestamp bigint,
-      updated_at bigint,
-      deleted BOOLEAN DEFAULT FALSE,
+      group_id INTEGER NOT NULL,
+      total DECIMAL(10, 2) NOT NULL,
+      paid_by VARCHAR(32) NOT NULL,
+      created_by VARCHAR(32) NOT NULL,
+      name VARCHAR(512) NOT NULL,
+      timestamp bigint NOT NULL,
+      updated_at bigint NOT NULL,
+      deleted BOOLEAN NOT NULL DEFAULT FALSE,
 
       FOREIGN KEY (group_id) REFERENCES groups(id),
       FOREIGN KEY (paid_by) REFERENCES users(id),
@@ -60,8 +61,8 @@ export async function createDatabase(pool: Pool) {
   await pool.query(`
     CREATE TABLE IF NOT EXISTS split_participants(
       split_id INTEGER,
-      user_id VARCHAR(32),
-      change DECIMAL(10, 2),
+      user_id VARCHAR(32) NOT NULL,
+      change DECIMAL(10, 2) NOT NULL,
 
       PRIMARY KEY (split_id, user_id),
       FOREIGN KEY (split_id) REFERENCES splits(id),
