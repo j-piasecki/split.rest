4ab8514d1b79925a6ed67abfdc2a2feb1cfa5158
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-12-07 13:56:33 +0100

    Add tanstack query

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   extends: ['plugin:@typescript-eslint/recommended', 'expo'],
   ignorePatterns: ['/dist/*'],
+  plugins: ['@tanstack/eslint-plugin-query'],
   rules: {
     'import/no-unresolved': 0,
   },
