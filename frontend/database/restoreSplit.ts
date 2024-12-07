239377c1d8d8e3f3047daa004ec53d72f7a85e64
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-12-07 21:27:41 +0100

    Add comment

--- frontend/database/restoreSplit.ts
+++ frontend/database/restoreSplit.ts
@@ -1,6 +1,7 @@
 import { makeRequest } from '../utils/makeApiRequest'
 import { RestoreSplitArguments, SplitWithChanges } from 'shared'
 
+// TODO: mutation I guess
 export async function restoreSplit(splitId: number, groupId: number): Promise<void> {
   const args: RestoreSplitArguments = { splitId, groupId }
 
