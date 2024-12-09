62c1fdbe431b9e05d8604423972e5766d6919164
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-12-09 20:25:32 +0100

    Handle not fount paths

---
+++ frontend/app/+not-found.tsx
@@ -0,0 +1,26 @@
+import Header from '@components/Header'
+import { useTheme } from '@styling/theme'
+import { useAuth } from '@utils/auth'
+import { ActivityIndicator, View, Text } from 'react-native'
+
+export default function NotFound() {
+  const user = useAuth()
+  const theme = useTheme()
+  
+  if (!user) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator color={theme.colors.primary} />
+      </View>
+    )
+  }
+
+  return (
+    <View style={{ flex: 1, backgroundColor: theme.colors.surface }}>
+      <Header />
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ color: theme.colors.onSurface, fontSize: 32 }}>{':('}</Text>
+      </View>
+    </View>
+  )
+}
