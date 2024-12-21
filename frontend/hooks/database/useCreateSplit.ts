8f0722c4ca69b380207f1afb4be106e6a8806bac
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-12-21 16:51:37 +0100

    Fix type missing

--- frontend/hooks/database/useCreateSplit.ts
+++ frontend/hooks/database/useCreateSplit.ts
@@ -2,7 +2,7 @@ import { useMutation } from '@tanstack/react-query'
 import { auth } from '@utils/firebase'
 import { makeRequest } from '@utils/makeApiRequest'
 import { addCachedSplit, invalidateSplitRelatedQueries } from '@utils/queryClient'
-import { CreateSplitArguments } from 'shared'
+import { CreateSplitArguments, SplitType } from 'shared'
 
 async function createSplit(args: CreateSplitArguments) {
   const splitId = await makeRequest<CreateSplitArguments, number>('POST', 'createSplit', args)
@@ -15,6 +15,7 @@ async function createSplit(args: CreateSplitArguments) {
     paidById: args.paidBy,
     updatedAt: Date.now(),
     total: String(args.total),
+    type: SplitType.Normal,
   })
 
   await invalidateSplitRelatedQueries(args.groupId, splitId ?? -1)
