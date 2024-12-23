7632e1b1254193ac475c9cd48638f89327b4f1e9
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-12-23 12:53:10 +0100

    Update 404 page

--- frontend/app/+not-found.tsx
+++ frontend/app/+not-found.tsx
@@ -1,26 +1,25 @@
 import Header from '@components/Header'
 import { Text } from '@components/Text'
 import { useTheme } from '@styling/theme'
-import { useAuth } from '@utils/auth'
-import { ActivityIndicator, View } from 'react-native'
+import { View } from 'react-native'
 
 export default function NotFound() {
-  const user = useAuth()
   const theme = useTheme()
-
-  if (!user) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator color={theme.colors.primary} />
-      </View>
-    )
-  }
 
   return (
     <View style={{ flex: 1, backgroundColor: theme.colors.surface }}>
       <Header />
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ color: theme.colors.onSurface, fontSize: 32 }}>{':('}</Text>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: 16,
+          paddingHorizontal: 16,
+          paddingBottom: 48,
+        }}
+      >
+        <Text style={{ color: theme.colors.outline, fontSize: 48 }}>{':('}</Text>
       </View>
     </View>
   )
