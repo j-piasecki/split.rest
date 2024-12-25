f4149ffd7b05a99e326a2c80501becf51ece0c6f
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-12-25 14:00:10 +0100

    Update people picker

--- frontend/components/PeoplePicker.tsx
+++ frontend/components/PeoplePicker.tsx
@@ -1,11 +1,14 @@
+import { Icon } from './Icon'
 import { RoundIconButton } from '@components/RoundIconButton'
 import { TextInputUserPicker } from '@components/TextInputUserPicker'
+import { useTheme } from '@styling/theme'
 import React from 'react'
-import { View } from 'react-native'
+import { Pressable, View } from 'react-native'
 import { User } from 'shared'
 
 export interface PersonEntry {
   email: string
+  selected?: boolean
   user?: User
 }
 
@@ -13,9 +16,17 @@ export interface PeoplePickerProps {
   groupId: number
   entries: PersonEntry[]
   onEntriesChange: (entries: PersonEntry[]) => void
+  selectable?: boolean
 }
 
-export function PeoplePicker({ groupId, entries, onEntriesChange }: PeoplePickerProps) {
+export function PeoplePicker({
+  groupId,
+  entries,
+  onEntriesChange,
+  selectable = false,
+}: PeoplePickerProps) {
+  const theme = useTheme()
+
   function cleanupEntries(entries: PersonEntry[]): PersonEntry[] {
     const newEntries = entries.filter((entry) => entry.email.trim() !== '')
 
@@ -24,6 +35,9 @@ export function PeoplePicker({ groupId, entries, onEntriesChange }: PeoplePicker
     }
     if (newEntries[newEntries.length - 1].email !== '') {
       newEntries.push({ email: '' })
+    }
+    if (selectable && newEntries.every((entry) => !entry.selected)) {
+      newEntries[0].selected = true
     }
 
     return newEntries
@@ -34,7 +48,7 @@ export function PeoplePicker({ groupId, entries, onEntriesChange }: PeoplePicker
   }
 
   return (
-    <View>
+    <View style={{ gap: 8 }}>
       {entries.map((entry, index) => {
         const deleteVisible = entry.email.trim().length > 0
         return (
@@ -46,6 +60,21 @@ export function PeoplePicker({ groupId, entries, onEntriesChange }: PeoplePicker
               zIndex: entries.length - index,
             }}
           >
+            {selectable && (
+              <Pressable
+                disabled={entry.selected || entry.email.indexOf('@') === -1}
+                onPress={() => setEntries(entries.map((e, i) => ({ ...e, selected: i === index })))}
+                style={{ marginRight: 8 }}
+                tabIndex={-1}
+              >
+                <Icon
+                  name='currency'
+                  size={24}
+                  color={entry.selected ? theme.colors.secondary : theme.colors.outlineVariant}
+                />
+              </Pressable>
+            )}
+
             <TextInputUserPicker
               key={index}
               groupId={groupId}
@@ -61,19 +90,23 @@ export function PeoplePicker({ groupId, entries, onEntriesChange }: PeoplePicker
               }
               onChangeText={(val) => {
                 const newEntries = [...entries]
-                newEntries[index] = { email: val, user: undefined }
+                newEntries[index] = { email: val, user: undefined, selected: entry.selected }
 
                 setEntries(newEntries)
               }}
               onSuggestionSelect={(user) => {
                 const newEntries = [...entries]
-                newEntries[index] = { email: user.email, user }
+                newEntries[index] = { email: user.email, user, selected: entry.selected }
 
                 setEntries(newEntries)
               }}
               onClearSelection={() => {
                 const newEntries = [...entries]
-                newEntries[index] = { email: entry.email, user: undefined }
+                newEntries[index] = {
+                  email: entry.email,
+                  user: undefined,
+                  selected: entry.selected,
+                }
 
                 setEntries(newEntries)
               }}
