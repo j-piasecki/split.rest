d20e948362b28ee05d24b8f223614597cc202e95
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-06-22 15:04:20 +0200

    Add number of pending invites to the button

--- frontend/components/homeScreen/InvitationsButton.tsx
+++ frontend/components/homeScreen/InvitationsButton.tsx
@@ -1,9 +1,11 @@
 import { PaneButton } from '@components/PaneButton'
 import { ShimmerPlaceholder } from '@components/ShimmerPlaceholder'
+import { Text } from '@components/Text'
 import { useTheme } from '@styling/theme'
 import { router } from 'expo-router'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { View } from 'react-native'
 import { GroupInviteWithGroupInfo } from 'shared'
 
 export interface InvitationsButtonProps {
@@ -26,6 +28,24 @@ export function InvitationsButton({ invites, isLoadingInvites }: InvitationsButt
           onPress={() => router.push('/groupInvites')}
           icon='stackedEmail'
           title={invites.length === 0 ? t('home.noGroupInvitesButton') : t('home.showGroupInvites')}
+          rightComponent={
+            invites.length > 0 && (
+              <View
+                style={{
+                  width: 26,
+                  height: 26,
+                  backgroundColor: theme.colors.error,
+                  borderRadius: 13,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Text style={{ color: theme.colors.onError, fontSize: 15, fontWeight: '700' }}>
+                  {invites.length > 9 ? '9+' : invites.length}
+                </Text>
+              </View>
+            )
+          }
         />
       )}
     </ShimmerPlaceholder>
