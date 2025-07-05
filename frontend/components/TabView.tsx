e23278014ab6892cc6b3d8726e8950f14f3ba351
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-07-05 21:23:22 +0200

    Springify tab view

--- frontend/components/TabView.tsx
+++ frontend/components/TabView.tsx
@@ -3,11 +3,103 @@ import { Text } from './Text'
 import { useTheme } from '@styling/theme'
 import React, { useEffect, useState } from 'react'
 import { Pressable, StyleProp, View, ViewStyle } from 'react-native'
+import Animated, { useAnimatedStyle, withSpring } from 'react-native-reanimated'
 
 export interface Tab {
   icon: IconName
   title: string
   content: React.ReactNode
+}
+
+function TabButton({
+  tab,
+  selected,
+  onPress,
+}: {
+  tab: Tab
+  selected: boolean
+  onPress: () => void
+}) {
+  const theme = useTheme()
+  const [pressed, setPressed] = useState(false)
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      flex: withSpring(pressed ? 1.25 : 1, {
+        mass: 1,
+        stiffness: 200,
+        damping: 12,
+        restDisplacementThreshold: 0.0001,
+      }),
+    }
+  })
+
+  const animatedContentStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          scale: withSpring(pressed ? 1.05 : 1, {
+            mass: 1,
+            stiffness: 200,
+            damping: 12,
+            restDisplacementThreshold: 0.0001,
+          }),
+        },
+      ],
+    }
+  })
+
+  return (
+    <Animated.View style={[animatedStyle, { flex: 1 }]}>
+      <Pressable
+        style={({ pressed, hovered }) => {
+          return {
+            backgroundColor: pressed
+              ? theme.colors.surfaceContainerHighest
+              : hovered || selected
+                ? theme.colors.surfaceContainerHigh
+                : theme.colors.transparent,
+            flex: 1,
+
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderBottomWidth: 1,
+            borderColor: theme.colors.outlineVariant,
+          }
+        }}
+        onPressIn={() => setPressed(true)}
+        onPressOut={() => setPressed(false)}
+        onPress={onPress}
+      >
+        <Animated.View
+          style={[
+            animatedContentStyle,
+            {
+              flexDirection: 'row',
+              gap: 8,
+            },
+          ]}
+        >
+          <Icon
+            name={tab.icon}
+            size={20}
+            color={selected ? theme.colors.primary : theme.colors.outline}
+          />
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: 700,
+              color: selected ? theme.colors.primary : theme.colors.outline,
+            }}
+            numberOfLines={1}
+            adjustsFontSizeToFit
+          >
+            {tab.title}
+          </Text>
+        </Animated.View>
+      </Pressable>
+    </Animated.View>
+  )
 }
 
 export interface TabViewProps {
@@ -30,14 +122,19 @@ export function TabView(props: TabViewProps) {
   return (
     <View
       style={[
-        { borderWidth: 1, borderColor: theme.colors.outlineVariant, borderRadius: 12 },
+        {
+          borderWidth: 1,
+          borderColor: theme.colors.outlineVariant,
+          borderRadius: 12,
+          overflow: 'hidden',
+        },
         props.style,
       ]}
     >
       <View
         style={{
           width: '100%',
-          height: 40,
+          height: 48,
           flexDirection: 'row',
         }}
       >
@@ -45,53 +142,28 @@ export function TabView(props: TabViewProps) {
           const selected = index === item
 
           return (
-            <Pressable
+            <TabButton
               key={index}
-              style={({ pressed, hovered }) => {
-                return {
-                  backgroundColor: pressed
-                    ? theme.colors.surfaceContainerHighest
-                    : hovered || selected
-                      ? theme.colors.surfaceContainerHigh
-                      : theme.colors.transparent,
-                  flex: 1,
-                  flexDirection: 'row',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  borderTopLeftRadius: index === 0 ? 12 : 0,
-                  borderTopRightRadius: index === props.tabs.length - 1 ? 12 : 0,
-                  borderBottomWidth: 1,
-                  borderColor: theme.colors.outlineVariant,
-                }
-              }}
+              tab={tab}
+              selected={selected}
               onPress={() => {
                 setSelectedItem(index)
                 props.onTabChange?.(index)
               }}
-            >
-              <Icon
-                name={tab.icon}
-                size={20}
-                color={selected ? theme.colors.primary : theme.colors.outline}
-              />
-              <Text
-                style={{
-                  fontSize: 16,
-                  fontWeight: 700,
-                  color: selected ? theme.colors.primary : theme.colors.outline,
-                  marginLeft: 8,
-                }}
-                numberOfLines={1}
-                adjustsFontSizeToFit
-              >
-                {tab.title}
-              </Text>
-            </Pressable>
+            />
           )
         })}
       </View>
 
-      <View key={item} style={{ width: '100%', flex: 1, padding: 8 }}>
+      <View
+        key={item}
+        style={{
+          width: '100%',
+          flex: 1,
+          padding: 8,
+          backgroundColor: theme.colors.surfaceContainer,
+        }}
+      >
         {props.tabs[item].content}
       </View>
     </View>
