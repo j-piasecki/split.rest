bbec40c19a5610cf6fa24145900c4ff8cf6dd4a9
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-07-21 17:54:20 +0200

    Extract split method selector and implement multiple selection

--- frontend/app/group/[id]/addSplit/index.tsx
+++ frontend/app/group/[id]/addSplit/index.tsx
@@ -1,7 +1,6 @@
 import { Button } from '@components/Button'
-import { Icon, IconName } from '@components/Icon'
 import ModalScreen from '@components/ModalScreen'
-import { RoundIconButton } from '@components/RoundIconButton'
+import { SplitMethodSelector } from '@components/SplitMethodSelector'
 import { Text } from '@components/Text'
 import { useModalScreenInsets } from '@hooks/useModalScreenInsets'
 import { useTheme } from '@styling/theme'
@@ -11,193 +10,8 @@ import { SplitCreationContext } from '@utils/splitCreationContext'
 import { useLocalSearchParams, useRouter } from 'expo-router'
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Pressable, ScrollView, StyleSheet, View } from 'react-native'
-import Animated, { useAnimatedStyle, withSpring, withTiming } from 'react-native-reanimated'
+import { ScrollView, View } from 'react-native'
 import { SplitMethod } from 'shared'
-
-const SplitTypeCard = ({
-  title,
-  description,
-  icon,
-  selected,
-  onSelect,
-}: {
-  title: string
-  description: string
-  icon: IconName
-  selected: boolean
-  onSelect: () => void
-}) => {
-  const theme = useTheme()
-  const threeBarLayout = useThreeBarLayout()
-  const [pressed, setPressed] = useState(false)
-  const [hovered, setHovered] = useState(false)
-  const [expanded, setExpanded] = useState(true)
-
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      opacity: withTiming(selected ? 0.9 : pressed ? 0.65 : hovered ? 0.3 : 0, {
-        duration: 200,
-      }),
-    }
-  })
-
-  const animatedContainerStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        {
-          scale: withSpring(pressed ? 1.025 : 1, {
-            damping: 15,
-            stiffness: 200,
-            restSpeedThreshold: 0.0001,
-          }),
-        },
-      ],
-    }
-  })
-
-  return (
-    <Pressable
-      onPressIn={() => setPressed(true)}
-      onPressOut={() => setPressed(false)}
-      onHoverIn={() => setHovered(true)}
-      onHoverOut={() => setHovered(false)}
-      onPress={onSelect}
-    >
-      <Animated.View
-        style={[
-          animatedContainerStyle,
-          {
-            gap: 8,
-            paddingVertical: 16,
-            paddingHorizontal: 12,
-            borderRadius: 16,
-            borderWidth: 1,
-            borderColor: theme.colors.outline,
-            overflow: 'hidden',
-            // @ts-expect-error - userSelect is not a valid style property
-            userSelect: 'none',
-          },
-        ]}
-      >
-        <Animated.View
-          style={[
-            StyleSheet.absoluteFill,
-            animatedStyle,
-            {
-              backgroundColor: theme.colors.secondaryContainer,
-            },
-          ]}
-        />
-
-        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 8, flex: 1 }}>
-          <Icon
-            name={icon}
-            size={threeBarLayout ? 20 : 24}
-            color={theme.colors.onSecondaryContainer}
-          />
-          <Text
-            style={{
-              fontSize: threeBarLayout ? 18 : 20,
-              fontWeight: 800,
-              color: theme.colors.onSecondaryContainer,
-            }}
-          >
-            {title}
-          </Text>
-
-          <View style={{ position: 'absolute', right: 0 }}>
-            <RoundIconButton
-              icon={expanded ? 'arrowUp' : 'arrowDown'}
-              size={threeBarLayout ? 20 : 24}
-              color={theme.colors.onSecondaryContainer}
-              onPress={() => setExpanded(!expanded)}
-            />
-          </View>
-        </View>
-        {expanded && (
-          <Text
-            style={{
-              marginLeft: 32,
-              fontSize: threeBarLayout ? 14 : 16,
-              fontWeight: 600,
-              color: theme.colors.onSurface,
-            }}
-          >
-            {description}
-          </Text>
-        )}
-      </Animated.View>
-    </Pressable>
-  )
-}
-
-function ConcreteSplitTypeCard({
-  method,
-  selectedMethod,
-  onSelect,
-}: {
-  method: SplitMethod
-  selectedMethod: SplitMethod
-  onSelect: (method: SplitMethod) => void
-}) {
-  const { t } = useTranslation()
-
-  switch (method) {
-    case SplitMethod.Equal:
-      return (
-        <SplitTypeCard
-          title={t('splitType.equalAmounts')}
-          description={t('splitTypeDescription.equalAmounts')}
-          icon='equal'
-          selected={selectedMethod === SplitMethod.Equal}
-          onSelect={() => onSelect(SplitMethod.Equal)}
-        />
-      )
-    case SplitMethod.BalanceChanges:
-      return (
-        <SplitTypeCard
-          title={t('splitType.balanceChanges')}
-          description={t('splitTypeDescription.balanceChanges')}
-          icon='barChart'
-          selected={selectedMethod === SplitMethod.BalanceChanges}
-          onSelect={() => onSelect(SplitMethod.BalanceChanges)}
-        />
-      )
-    case SplitMethod.ExactAmounts:
-      return (
-        <SplitTypeCard
-          title={t('splitType.exactAmounts')}
-          description={t('splitTypeDescription.exactAmounts')}
-          icon='exactAmount'
-          selected={selectedMethod === SplitMethod.ExactAmounts}
-          onSelect={() => onSelect(SplitMethod.ExactAmounts)}
-        />
-      )
-    case SplitMethod.Lend:
-      return (
-        <SplitTypeCard
-          title={t('splitType.lend')}
-          description={t('splitTypeDescription.lend')}
-          icon='payment'
-          selected={selectedMethod === SplitMethod.Lend}
-          onSelect={() => onSelect(SplitMethod.Lend)}
-        />
-      )
-    case SplitMethod.Delayed:
-      return (
-        <SplitTypeCard
-          title={t('splitType.delayed')}
-          description={t('splitTypeDescription.delayed')}
-          icon='schedule'
-          selected={selectedMethod === SplitMethod.Delayed}
-          onSelect={() => onSelect(SplitMethod.Delayed)}
-        />
-      )
-    default:
-      return null
-  }
-}
 
 export default function Modal() {
   const theme = useTheme()
@@ -240,14 +54,12 @@ export default function Modal() {
             {t('splitType.selectType')}
           </Text>
 
-          {SplitCreationContext.current.allowedSplitMethods.map((method) => (
-            <ConcreteSplitTypeCard
-              key={method}
-              method={method}
-              selectedMethod={selectedSplitType}
-              onSelect={setSelectedSplitType}
-            />
-          ))}
+          <SplitMethodSelector
+            allowedMethods={SplitCreationContext.current.allowedSplitMethods}
+            multiple={false}
+            selectedMethod={selectedSplitType}
+            onSelect={setSelectedSplitType}
+          />
         </ScrollView>
 
         <View style={{ paddingLeft: insets.left + 12, paddingRight: insets.right + 12 }}>
---
+++ frontend/components/SplitMethodSelector.tsx
@@ -0,0 +1,263 @@
+import { Icon, IconName } from '@components/Icon'
+import { RoundIconButton } from '@components/RoundIconButton'
+import { Text } from '@components/Text'
+import { useTheme } from '@styling/theme'
+import { useThreeBarLayout } from '@utils/dimensionUtils'
+import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { Pressable, StyleSheet, View } from 'react-native'
+import Animated, { useAnimatedStyle, withSpring, withTiming } from 'react-native-reanimated'
+import { SplitMethod } from 'shared'
+
+const OrderedSplitMethods = [
+  SplitMethod.Equal,
+  SplitMethod.ExactAmounts,
+  SplitMethod.BalanceChanges,
+  SplitMethod.Lend,
+  SplitMethod.Delayed,
+]
+
+const SplitTypeCard = ({
+  title,
+  description,
+  icon,
+  selected,
+  onSelect,
+  startExpanded,
+}: {
+  title: string
+  description: string
+  icon: IconName
+  selected: boolean
+  startExpanded: boolean
+  onSelect: () => void
+}) => {
+  const theme = useTheme()
+  const threeBarLayout = useThreeBarLayout()
+  const [pressed, setPressed] = useState(false)
+  const [hovered, setHovered] = useState(false)
+  const [expanded, setExpanded] = useState(startExpanded)
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: withTiming(selected ? 0.9 : pressed ? 0.65 : hovered ? 0.3 : 0, {
+        duration: 200,
+      }),
+    }
+  })
+
+  const animatedContainerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          scale: withSpring(pressed ? 1.025 : 1, {
+            damping: 15,
+            stiffness: 200,
+            restSpeedThreshold: 0.0001,
+          }),
+        },
+      ],
+    }
+  })
+
+  return (
+    <Pressable
+      onPressIn={() => setPressed(true)}
+      onPressOut={() => setPressed(false)}
+      onHoverIn={() => setHovered(true)}
+      onHoverOut={() => setHovered(false)}
+      onPress={onSelect}
+    >
+      <Animated.View
+        style={[
+          animatedContainerStyle,
+          {
+            gap: 8,
+            paddingVertical: 16,
+            paddingHorizontal: 12,
+            borderRadius: 16,
+            borderWidth: 1,
+            borderColor: theme.colors.outline,
+            overflow: 'hidden',
+            // @ts-expect-error - userSelect is not a valid style property
+            userSelect: 'none',
+          },
+        ]}
+      >
+        <Animated.View
+          style={[
+            StyleSheet.absoluteFill,
+            animatedStyle,
+            {
+              backgroundColor: theme.colors.secondaryContainer,
+            },
+          ]}
+        />
+
+        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 8, flex: 1 }}>
+          <Icon
+            name={icon}
+            size={threeBarLayout ? 20 : 24}
+            color={theme.colors.onSecondaryContainer}
+          />
+          <Text
+            style={{
+              fontSize: threeBarLayout ? 18 : 20,
+              fontWeight: 800,
+              color: theme.colors.onSecondaryContainer,
+            }}
+          >
+            {title}
+          </Text>
+
+          <View style={{ position: 'absolute', right: 0 }}>
+            <RoundIconButton
+              icon={expanded ? 'arrowUp' : 'arrowDown'}
+              size={threeBarLayout ? 20 : 24}
+              color={theme.colors.onSecondaryContainer}
+              onPress={() => setExpanded(!expanded)}
+            />
+          </View>
+        </View>
+        {expanded && (
+          <Text
+            style={{
+              marginLeft: 32,
+              fontSize: threeBarLayout ? 14 : 16,
+              fontWeight: 600,
+              color: theme.colors.onSurface,
+            }}
+          >
+            {description}
+          </Text>
+        )}
+      </Animated.View>
+    </Pressable>
+  )
+}
+
+function ConcreteSplitTypeCard({
+  method,
+  selectedMethods,
+  onSelect,
+  startExpanded = true,
+}: {
+  method: SplitMethod
+  selectedMethods: SplitMethod[]
+  onSelect: (method: SplitMethod) => void
+  startExpanded?: boolean
+}) {
+  const { t } = useTranslation()
+
+  switch (method) {
+    case SplitMethod.Equal:
+      return (
+        <SplitTypeCard
+          title={t('splitType.equalAmounts')}
+          description={t('splitTypeDescription.equalAmounts')}
+          icon='equal'
+          selected={selectedMethods.includes(SplitMethod.Equal)}
+          onSelect={() => onSelect(SplitMethod.Equal)}
+          startExpanded={startExpanded}
+        />
+      )
+    case SplitMethod.BalanceChanges:
+      return (
+        <SplitTypeCard
+          title={t('splitType.balanceChanges')}
+          description={t('splitTypeDescription.balanceChanges')}
+          icon='barChart'
+          selected={selectedMethods.includes(SplitMethod.BalanceChanges)}
+          onSelect={() => onSelect(SplitMethod.BalanceChanges)}
+          startExpanded={startExpanded}
+        />
+      )
+    case SplitMethod.ExactAmounts:
+      return (
+        <SplitTypeCard
+          title={t('splitType.exactAmounts')}
+          description={t('splitTypeDescription.exactAmounts')}
+          icon='exactAmount'
+          selected={selectedMethods.includes(SplitMethod.ExactAmounts)}
+          onSelect={() => onSelect(SplitMethod.ExactAmounts)}
+          startExpanded={startExpanded}
+        />
+      )
+    case SplitMethod.Lend:
+      return (
+        <SplitTypeCard
+          title={t('splitType.lend')}
+          description={t('splitTypeDescription.lend')}
+          icon='payment'
+          selected={selectedMethods.includes(SplitMethod.Lend)}
+          onSelect={() => onSelect(SplitMethod.Lend)}
+          startExpanded={startExpanded}
+        />
+      )
+    case SplitMethod.Delayed:
+      return (
+        <SplitTypeCard
+          title={t('splitType.delayed')}
+          description={t('splitTypeDescription.delayed')}
+          icon='schedule'
+          selected={selectedMethods.includes(SplitMethod.Delayed)}
+          onSelect={() => onSelect(SplitMethod.Delayed)}
+          startExpanded={startExpanded}
+        />
+      )
+    default:
+      return null
+  }
+}
+
+interface BaseSplitMethodSelectorProps {
+  allowedMethods: SplitMethod[]
+  startExpanded?: boolean
+}
+
+interface SingleSplitMethodSelectorProps extends BaseSplitMethodSelectorProps {
+  multiple: false
+  selectedMethod: SplitMethod
+  onSelect: (method: SplitMethod) => void
+}
+
+interface MultipleSplitMethodSelectorProps extends BaseSplitMethodSelectorProps {
+  multiple: true
+  selectedMethods: SplitMethod[]
+  onSelectionChange: (methods: SplitMethod[]) => void
+}
+
+export type SplitMethodSelectorProps =
+  | SingleSplitMethodSelectorProps
+  | MultipleSplitMethodSelectorProps
+
+export function SplitMethodSelector(props: SplitMethodSelectorProps) {
+  const selectedMethods = props.multiple ? props.selectedMethods : [props.selectedMethod]
+
+  function onSelect(method: SplitMethod) {
+    if (props.multiple) {
+      const newSelectedMethods = selectedMethods.includes(method)
+        ? selectedMethods.filter((m) => m !== method)
+        : [...selectedMethods, method]
+      props.onSelectionChange(newSelectedMethods)
+    } else {
+      props.onSelect(method)
+    }
+  }
+
+  return (
+    <View style={{ gap: 16 }}>
+      {OrderedSplitMethods.filter((method) => props.allowedMethods.includes(method)).map(
+        (method) => (
+          <ConcreteSplitTypeCard
+            key={method}
+            method={method}
+            selectedMethods={selectedMethods}
+            onSelect={onSelect}
+            startExpanded={props.startExpanded}
+          />
+        )
+      )}
+    </View>
+  )
+}
