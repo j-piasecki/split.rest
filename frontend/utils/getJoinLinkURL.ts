5bfa606c0acc8bb08bce3671ea833226aa6eb2d9
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-08-03 20:38:22 +0200

    Extract constants

--- frontend/utils/getJoinLinkURL.ts
+++ frontend/utils/getJoinLinkURL.ts
@@ -1,9 +1,9 @@
-import { GroupJoinLink } from "shared";
+import { GroupJoinLink } from 'shared'
 
 export function getJoinLinkURL(link: GroupJoinLink | undefined | null) {
   const linkText = __DEV__
     ? `http://localhost:8081/join/${link?.uuid}`
     : `https://split.rest/join/${link?.uuid}`
 
-  return linkText;
+  return linkText
 }
