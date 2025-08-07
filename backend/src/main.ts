a55a67e01617bf7bf24411914e155c69cfa62a6a
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-08-07 19:36:14 +0200

    Don't start directly on 443

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,28 +4,17 @@ import { NestExpressApplication } from '@nestjs/platform-express'
 import * as fs from 'fs'
 import { join } from 'path'
 
-const IS_DEV = process.env.DEV === '1'
-
-const httpsOptions = IS_DEV
-  ? undefined
-  : {
-      key: fs.readFileSync('./secrets/private-key.pem'),
-      cert: fs.readFileSync('./secrets/public-certificate.pem'),
-    }
-
 if (!fs.existsSync('./public')) {
   fs.mkdirSync('./public')
 }
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    httpsOptions: httpsOptions,
-  })
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.useStaticAssets(join(__dirname, '..', 'public'), {
     prefix: '/public',
     maxAge: '7d',
   })
   app.enableCors()
-  await app.listen(IS_DEV ? 3000 : 443)
+  await app.listen(3000)
 }
 bootstrap()
