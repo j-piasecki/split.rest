1cde1438edcf9670b3fa944f8ee6822f3fb9bf10
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-08-16 14:42:35 +0200

    Update profile picture util

--- backend/src/profilePicture.ts
+++ backend/src/profilePicture.ts
@@ -1,10 +1,14 @@
 import { DeleteObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import * as fs from 'fs'
+import sharp from 'sharp'
 
 export async function downloadProfilePicture(url: string, id: string) {
   const photo = await fetch(url)
   const buffer = await photo.arrayBuffer()
-  fs.writeFileSync(`public/${id}.png`, Buffer.from(buffer))
+  await sharp(Buffer.from(buffer))
+    .resize(128, 128)
+    .toFormat('jpg', { quality: 80 })
+    .toFile(`public/${id}.jpg`)
 }
 
 export async function downloadProfilePictureToBase64(url: string, id: string) {
@@ -12,24 +16,24 @@ export async function downloadProfilePictureToBase64(url: string, id: string) {
   const contentType = photo.headers.get('content-type')
   const buffer = await photo.arrayBuffer()
 
-  if (!fs.existsSync(`public/${id}.png`)) {
-    fs.writeFileSync(`public/${id}.png`, Buffer.from(buffer))
+  if (!fs.existsSync(`public/${id}.jpg`)) {
+    fs.writeFileSync(`public/${id}.jpg`, Buffer.from(buffer))
   }
 
   return 'data:' + contentType + ';base64,' + Buffer.from(buffer).toString('base64')
 }
 
 export async function deleteProfilePicture(id: string) {
-  fs.unlinkSync(`public/${id}.png`)
+  fs.unlinkSync(`public/${id}.jpg`)
 }
 
 export async function uploadProfilePictureToR2(s3Client: S3Client, bucketName: string, id: string) {
-  const file = fs.readFileSync(`public/${id}.png`)
+  const file = fs.readFileSync(`public/${id}.jpg`)
   const uploadCommand = new PutObjectCommand({
     Bucket: bucketName,
-    Key: `profile-pictures/${id}.png`,
+    Key: `profile-pictures/${id}.jpg`,
     Body: file,
-    ContentType: 'image/png',
+    ContentType: 'image/jpg',
   })
   await s3Client.send(uploadCommand)
 }
@@ -41,7 +45,7 @@ export async function deleteProfilePictureFromR2(
 ) {
   const deleteCommand = new DeleteObjectCommand({
     Bucket: bucketName,
-    Key: `profile-pictures/${id}.png`,
+    Key: `profile-pictures/${id}.jpg`,
   })
   await s3Client.send(deleteCommand)
 }
