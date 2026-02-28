669676d81828574053b8ba02220993b318e4b3ec
Jakub Piasecki <jakub.piasecki@swmansion.com>
2026-02-28 14:22:33 +0100

    Update animation config

--- frontend/styling/animationConfigs.ts
+++ frontend/styling/animationConfigs.ts
@@ -1,3 +1,3 @@
-export const buttonCornerSpringConfig = { mass: 1, stiffness: 250, damping: 12 } as const
+export const buttonCornerSpringConfig = { mass: 1, stiffness: 250, damping: 15 } as const
 export const buttonPaddingSpringConfig = { mass: 1, stiffness: 300, damping: 25 } as const
 export const drawerSpringConfig = { mass: 1, damping: 50, stiffness: 400 } as const
