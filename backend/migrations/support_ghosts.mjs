a09f6f4bccc4f0461a495746acf093622a50fa69
Jakub Piasecki <jakub.piasecki@swmansion.com>
2026-03-07 19:51:05 +0100

    Add migration script

---
+++ backend/migrations/support_ghosts.mjs
@@ -0,0 +1,27 @@
+import pg from 'pg'
+
+const pool = new pg.Pool({
+  user: process.env.SPLIT_USER,
+  password: process.env.SPLIT_PASSWORD,
+  host: process.env.SPLIT_HOST,
+  port: Number(process.env.SPLIT_PORT),
+  database: process.env.SPLIT_DATABASE,
+})
+
+async function main() {
+  const client = await pool.connect()
+
+  try {
+    await client.query('ALTER TABLE users ADD COLUMN is_ghost BOOLEAN NOT NULL DEFAULT FALSE')
+    await client.query(
+      'ALTER TABLE users ADD CONSTRAINT users_id_is_ghost_unique UNIQUE (id, is_ghost)'
+    )
+  } catch (e) {
+    console.error(e)
+    await client.query('ROLLBACK')
+  } finally {
+    client.release()
+  }
+}
+
+main()
