949265c912b9cf395250014335b74a7c0ffcf86a
Jakub Piasecki <jakub.piasecki@swmansion.com>
2026-03-27 18:30:33 +0100

    Invalidate group info when adding ghost

--- frontend/hooks/database/useCreateGhostMutation.ts
+++ frontend/hooks/database/useCreateGhostMutation.ts
@@ -1,6 +1,6 @@
 import { useMutation } from '@tanstack/react-query'
 import { makeRequest } from '@utils/makeApiRequest'
-import { invalidateGroupMembers } from '@utils/queryClient'
+import { invalidateGroupInfo, invalidateGroupMembers } from '@utils/queryClient'
 import { CreateGhostArguments, Member, TranslatableError } from 'shared'
 
 async function createGhost(groupId: number, name: string) {
@@ -13,6 +13,7 @@ async function createGhost(groupId: number, name: string) {
   }
 
   await invalidateGroupMembers(groupId)
+  await invalidateGroupInfo(groupId)
 
   return response
 }
